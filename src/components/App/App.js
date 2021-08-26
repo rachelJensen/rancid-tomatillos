cb8f8c468365d7ab69f2f41c0e1d5db1b1350609
StevenTheBearded <stevenberg22@gmail.com>
2021-08-26 17:01:23 -0400

    Create method to utilize apicalls

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,29 @@
-import movieData from '../../movieData';
+// import movieData from '../../movieData';
 import './App.css';
 import React, { Component } from 'react';
 import Header from '../Header/Header';
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
 import MovieDetailer from '../MovieDetailer/MovieDetailer';
+import { allMovies, singleMovie } from '../../apiCalls'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: movieData.movies,
+      movies: [],
       movieID: 0
     };
   }
 
+  componentDidMount = () => {
+    allMovies()
+      .then(data => this.setState({ movies: data.movies}))
+  }
+
   findMovie = (id) => {
     this.setState({movieID: id})
+    singleMovie(id)
+      .then(data => console.log('singlemovie :', data.movie))
   }
 
   render() {
@@ -23,7 +31,7 @@ class App extends Component {
       <main>
         <Header />
         {!this.state.movieID &&  <MoviesContainer movies={this.state.movies} findMovie={this.findMovie} />}
-        {this.state.movieID && <MovieDetailer />}
+        {this.state.movieID && <MovieDetailer  />}
       </main>
     );
   }
