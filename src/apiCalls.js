a81235f650851dc98d2b84a4114572aeecc7ec90
StevenTheBearded <stevenberg22@gmail.com>
2021-08-26 17:01:03 -0400

    Create apiCalls

---
+++ src/apiCalls.js
@@ -0,0 +1,11 @@
+export const allMovies = () => {
+  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+    .then(res => res.json())
+    .then(data => data)
+}
+
+export const singleMovie = (id) => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+    .then(res => res.json())
+    .then(data => data)
+}
