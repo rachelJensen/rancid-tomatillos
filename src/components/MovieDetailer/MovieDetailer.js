b607e07a93a32a9b3822754eb11cc1b85f102bff
StevenTheBearded <stevenberg22@gmail.com>
2021-08-26 17:03:58 -0400

    Rename prop

--- src/components/MovieDetailer/MovieDetailer.js
+++ src/components/MovieDetailer/MovieDetailer.js
@@ -3,15 +3,15 @@ import './MovieDetailer.css';
 const MovieDetailer = ({ movieID }) => {
   return (
     <section>
-      <img className='backdrop' src={movieID.backdrop_path} alt={fmovieID.title}/>
-      <h2>{fmovieID.title}</h2>
-      <p className='release'>{fmovieID.release_date}</p>
-      <p className='overview'>{fmovieID.overview}</p>
-      <p className='rating'>{fmovieID.average_rating}</p>
-      <p className='budget'>{fmovieID.budget}</p>
+      <img className='backdrop' src={movieID.backdrop_path} alt={movieID.title}/>
+      <h2>{movieID.title}</h2>
+      <p className='release'>{movieID.release_date}</p>
+      <p className='overview'>{movieID.overview}</p>
+      <p className='rating'>{movieID.average_rating}</p>
+      <p className='budget'>{movieID.budget}</p>
       <p className='genres'>Drama, Comedy, Whatever</p>
-      <p className='runtime'>{fmovieID.runtime}</p>
-      <p className='tagline'>{fmovieID.tagline}</p>
+      <p className='runtime'>{movieID.runtime}</p>
+      <p className='tagline'>{movieID.tagline}</p>
     </section>
   )
 }
