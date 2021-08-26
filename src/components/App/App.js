3bf216f6805dd61bbea4caa63e6374294616d5c1
Rachel Jensen <rachelangela@gmail.com>
2021-08-26 13:58:30 -0600

    Add conditional rendering

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,20 +3,27 @@ import './App.css';
 import React, { Component } from 'react';
 import Header from '../Header/Header';
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
+import MovieDetailer from '../MovieDetailer/MovieDetailer';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       movies: movieData.movies,
+      movieID: 0
     };
+  }
+
+  findMovie = (id) => {
+    this.setState({movieID: id})
   }
 
   render() {
     return (
       <main>
         <Header />
-        <MoviesContainer movies={this.state.movies} />
+        {!this.state.movieID &&  <MoviesContainer movies={this.state.movies} findMovie={this.findMovie} />}
+        {this.state.movieID && <MovieDetailer />}
       </main>
     );
   }
