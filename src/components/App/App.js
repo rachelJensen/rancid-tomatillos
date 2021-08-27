4f05048ff4ce46c74ba59d68ec58f2d4cc578ff3
StevenTheBearded <stevenberg22@gmail.com>
2021-08-27 17:14:52 -0400

    Create home button functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,30 +16,29 @@ class App extends Component {
     };
   }
 
+  reload = () => {
+    this.setState({ movieID: 0 })
+  }
+
   componentDidMount = () => {
     allMovies()
       .then(data => this.setState({ movies: data.movies}))
   }
 
   findMovie = (id) => {
-    // console.log('id <<<<<<:', id);
-    this.setState({movieID: id})// changes the conditional rendering for movieContainer
+    this.setState({movieID: id})
     this.hanldeSingleMovie(id)
   }
   
   hanldeSingleMovie = (id) => {
     singleMovie(id)
-      // .then(data => data)
       .then(data => this.setState({ movie: data.movie}))
-      // .then(data => console.log(data, ': DATA INSIDE HANDLESINGLEMOVIE'))
-      // .then(data => this.setState({ singleMovie: data }))
-      // .then(console.log(this.state.singleMovie))
   }
 
   render() {
     return (
       <main>
-        <Header />
+        <Header reload={this.reload} />
         {!this.state.movieID &&  <MoviesContainer movies={this.state.movies} findMovie={this.findMovie} />}
         {this.state.movieID && <MovieDetailer movie={this.state.movie} />}
       </main>
