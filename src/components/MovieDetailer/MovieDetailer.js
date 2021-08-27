66aee92e58b90f8fea8464edbe6716db9bde1048
Rachel Jensen <rachelangela@gmail.com>
2021-08-27 17:01:21 -0600

    Reorder movie element display

--- src/components/MovieDetailer/MovieDetailer.js
+++ src/components/MovieDetailer/MovieDetailer.js
@@ -5,16 +5,18 @@ const MovieDetailer = ({ movie }) => {
  
  
   return (
-    <section>
+    <section className='movie-info'>
       <img className='backdrop' src={movie.backdrop_path} alt={movie.title}/>
-      <h2>{movie.title}</h2>
-      <p className='release'>{movie.release_date}</p>
-      <p className='overview'>{movie.overview}</p>
-      <p className='rating'>{movie.average_rating}</p>
-      <p className='budget'>{movie.budget}</p>
-      <p className='genres'>Drama, Comedy, Whatever</p>
-      <p className='runtime'>{movie.runtime}</p>
-      <p className='tagline'>{movie.tagline}</p>
+      <div className='details-box'>
+        <h2>{movie.title}</h2>
+        <p className='tagline'>{movie.tagline}</p>
+        <p className='genres'>Drama, Comedy, Whatever</p>
+        <p className='release'>{movie.release_date}</p>
+        <p className='overview'>{movie.overview}</p>
+        <p className='rating'>{movie.average_rating}</p>
+        <p className='budget'>{movie.budget}</p>
+        <p className='runtime'>Runtime: {movie.runtime} min</p>
+      </div> 
     </section>
   )
 }
