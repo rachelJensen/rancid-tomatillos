711788250c7c37481b6a3d92047e2b2fc0780328
StevenTheBearded <stevenberg22@gmail.com>
2021-08-28 13:54:12 -0400

    create api call for movie videos

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,4 +8,11 @@ export const singleMovie = (id) => {
   return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
     .then(res => res.json())
     .then(data => data)
+  
+}
+
+export const singleMoviesVideos = (id) => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`)
+  .then(res => res.json())
+  .then(data => data)
 }
