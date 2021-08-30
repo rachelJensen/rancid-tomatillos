dd739852731cfc4b1aa346718c1a45998ee4de67
StevenTheBearded <stevenberg22@gmail.com>
2021-08-30 19:30:52 -0400

    Create fixtures blocks to stub request call

--- cypress/integration/landingPage_spec.js
+++ cypress/integration/landingPage_spec.js
@@ -1,9 +1,14 @@
-beforeEach(() => {
-  cy.visit('http://localhost:3000')
-})
+describe('Main Page rendering', () => {
+  beforeEach(() => {
+    cy.fixture('mockMovies').then((movies) => {
+      cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', movies)
+    })
+    cy.visit('http://localhost:3000');
+  });
 
-describe('Should be able to render main page', () => {
-  it('should contain a header', () => {
-    expect(true).to.be(true)
+  describe('Should be able to render main page', () => {
+    it('should contain a header', () => {
+      expect(true).to.equal(true)
+    })
   })
 })
