637a2ae1edb35bf232fb71e37378fee0a59ac50a
StevenTheBearded <stevenberg22@gmail.com>
2021-08-30 16:18:22 -0400

    Add functionality for error handling

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,8 +1,17 @@
 export const allMovies = () => {
-  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-  .then(res => res.json())
+  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/moes')
+  // .then(res => res.json())
+  .then(res => {
+    if(res.ok) {
+      return res.json()
+    } else {
+      throw new Error('TEST')
+    }
+  })
   .then(data => data)
 }
+
+// export const allMovies = await
 
 export const singleMovie = (id) => {
   return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
