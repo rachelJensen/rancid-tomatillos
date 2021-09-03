872c6ea9c2f1fa06ecae974a4efbeed0d40e5778
Rachel Jensen <rachelangela@gmail.com>
2021-09-03 14:50:43 -0600

    Convert MovieDetailer to class component

--- src/components/MovieDetailer/MovieDetailer.js
+++ src/components/MovieDetailer/MovieDetailer.js
@@ -1,27 +1,41 @@
 import './MovieDetailer.css';
-import { useEffect } from 'react';
+import React, { Component } from 'react';
+import { fetchData } from '../../apiCalls';
 
-const MovieDetailer = ({ movie, hanldeSingleMovie, location }) => {
+class MovieDetailer extends Component { 
+  constructor() {
+    super();
+    this.state = {
+      movie: {},
+      video: [],
+      error: ''
+    }
+  }
 
-  useEffect(() => {
-    hanldeSingleMovie(location)
-  }, [])
-
-  return (
-    <section className='movie-info'>
-      <img className='backdrop' src={movie.backdrop_path} alt={movie.title}/>
-      <div className='details-box'>
-        <h2>{movie.title}</h2>
-        <p className='tagline'>{movie.tagline}</p>
-        <p className='genres'>Drama, Comedy, Whatever</p>
-        <p className='release'>{movie.release_date}</p>
-        <p className='overview'>{movie.overview}</p>
-        <p className='rating'>{movie.average_rating}</p>
-        <p className='budget'>{movie.budget}</p>
-        <p className='runtime'>Runtime: {movie.runtime} min</p>
-      </div> 
-    </section>
-  )
+  componentDidMount = () => {
+    fetchData(this.props.id)
+        .then(data => this.setState({ movie: data.movie}))
+        .catch(err => this.setState({ error: err }))
+  }
+ 
+  render = () => {
+    
+    return (
+      <section className='movie-info'>
+        <img className='backdrop' src={this.state.movie.backdrop_path} alt={this.state.movie.title}/>
+        <div className='details-box'>
+          <h2>{this.state.movie.title}</h2>
+          <p className='tagline'>{this.state.movie.tagline}</p>
+          <p className='genres'>Drama, Comedy, Whatever</p>
+          <p className='release'>{this.state.movie.release_date}</p>
+          <p className='overview'>{this.state.movie.overview}</p>
+          <p className='rating'>{this.state.movie.average_rating}</p>
+          <p className='budget'>{this.state.movie.budget}</p>
+          <p className='runtime'>Runtime: {this.state.movie.runtime} min</p>
+        </div> 
+      </section>
+    )
+  }
 }
 
 export default MovieDetailer;
