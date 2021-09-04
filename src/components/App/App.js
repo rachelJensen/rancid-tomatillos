c7fd1cd8f71bb3c945ca4a7ac8471e11fb085bdd
StevenTheBearded <stevenberg22@gmail.com>
2021-09-04 16:24:48 -0400

    Remove import

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,6 @@ import MovieDetailer from '../MovieDetailer/MovieDetailer';
 import Error404 from '../Error404/Error404';
 import { fetchData } from '../../apiCalls';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import {formatMovieDetails} from '../../Utils'
 
 class App extends Component {
   constructor() {
@@ -31,7 +30,7 @@ class App extends Component {
             <Redirect to="/movies" />
           </Route>
           <Route exact path='/movies' render={() => 
-            <MoviesContainer movies={this.state.movies} findMovie={this.findMovie} />} />
+            <MoviesContainer movies={this.state.movies} />} />
           <Route exact path='/movies/:id' render={({ match }) => 
             <MovieDetailer id={parseInt(match.params.id)}/> }/>
           <Route path='*' render={() => 
@@ -43,30 +42,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-// - Styling for home page (Neon Lights)
-// - Refactor ApiCalls to one function
-// - Error Handling for ApiCalls
-// - Scrub Data off api call method inside app.js?
-// - Add videos to movieDetailer.js 
-// - Remove useEffect() from movieDetailer.js
-
-
-// findMovie = (id) => {
-//     this.setState({movieID: id})
-//   }
-  
-// hanldeSingleMovie = (id) => {
-//     fetchData(id)
-//       .then(data => this.setState({ movie: data.movie}))
-//       .catch(err => this.setState({error: err}))
-//     fetchData(`${id}/videos`)
-//       .then(data => console.log(data, ' :video data'))
-// }
-
-// error500 = () => {
-//   return ( 
-//     <h1>There is a problem with the server. Please reload or check back at a later time.</h1>//style me
-//   )
-// }
