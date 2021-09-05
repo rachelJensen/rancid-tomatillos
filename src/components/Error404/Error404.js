c3eafd9258ebc4270b22937d3eb0b02426be823b
Rachel Jensen <rachelangela@gmail.com>
2021-09-05 16:37:11 -0600

    Fix font error styling bleedover on movie page

--- src/components/Error404/Error404.js
+++ src/components/Error404/Error404.js
@@ -6,8 +6,8 @@ const Error404 = () => {
       <Link to="*" >
         <section className="error">
           <div className="error-message">
-            <h2>404</h2>
-            <h3>Page Not Found</h3>
+            <h2 className='neon-error'>404</h2>
+            <h3 className='neon-error'>Page Not Found</h3>
           </div>
         </section>
       </Link>
