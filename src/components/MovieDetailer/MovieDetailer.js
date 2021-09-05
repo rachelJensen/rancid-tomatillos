7adc9c5e1c5cd50f969760b07b48fb19d5500200
StevenTheBearded <stevenberg22@gmail.com>
2021-09-05 12:40:17 -0400

    Add react player to moviedetailer

--- src/components/MovieDetailer/MovieDetailer.js
+++ src/components/MovieDetailer/MovieDetailer.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { fetchData } from '../../apiCalls';
 import { formatMovieDetails } from '../../Utils';
 import { Redirect } from 'react-router-dom';
+import ReactPlayer from 'react-player/youtube';
 
 class MovieDetailer extends Component { 
   constructor() {
@@ -16,21 +17,37 @@ class MovieDetailer extends Component {
 
   componentDidMount = () => {
     fetchData(this.props.id)
-        .then(data => this.setState({ movie: formatMovieDetails(data.movie)}))
-        .catch(err => this.setState({ error: err }))
+      .then(data => this.setState({ movie: formatMovieDetails(data.movie)}))
+      .catch(err => this.setState({ error: err }))
     fetchData(`${this.props.id}/videos`)
-        .then(data => console.log({data}, ' :video data'))
-        .catch(err => console.log(err))
-        //decide how to handle if no video data comes back
+      .then(data => this.setState({ video: data.videos}))
+    // .then(data => console.log(this.state.video, ' :data inside moveidetailer'))
+      .catch(err => console.log(err))
+      //decide how to handle if no video data comes back
   }
+
+  // componentDidUpdate = () => {
+  //   if(this.state.video) {
+  //     console.log(this.state.video, ' :videostate inside if conditional')
+  //     console.log('videoExist logged if conditional');
+  //     return 
+  //   } else {
+  //     console.log(this.state.video, 'videoExist logged else conditional');
+      
+  //     return <ReactPlayer url={`https://www.youtube.com/watch?v=${this.state.video[0].key}`} />
+  //   }
+  // }
  
   render = () => {
     if (this.props.id === NaN || this.state.error) {
       return  <Redirect to='/not-found' />
     }
-
+    // 01ON04GCwKs
     return (
       <section className='movie-info'>
+        {console.log(this.state.video[0], 'this.state.video inside moviedetailer render')} 
+        {/* {this.videoExist()} */}
+        <ReactPlayer url={`https://www.youtube.com/watch?v=01ON04GCwKs`} />
         <img className='backdrop' src={this.state.movie.backdrop_path} alt={this.state.movie.title}/>
         <div className='details-box'>
           <h2>{this.state.movie.title}</h2>
