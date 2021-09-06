a285a90866e5dd942bcf9d615a9be7366803350b
Rachel Jensen <rachelangela@gmail.com>
2021-09-06 16:03:40 -0600

    Add server error test

--- cypress/integration/notFound_spec.js
+++ cypress/integration/notFound_spec.js
@@ -3,7 +3,6 @@ describe('404 not found page', () => {
     cy.fixture('mockSingleMovie').then((movie) => {
       cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/627290', movie)
     })
-    // cy.visit('http://localhost:3000/movies/622');
   });
   
   it('should redirect to the not found page if movie id is invalid', () => {
@@ -24,5 +23,41 @@ describe('404 not found page', () => {
     cy
       .get('h2').contains('404')
       .get('h3').contains('Page Not Found')
+  })
+})
+
+describe('server failure', () => {
+  it('should display an error message if the server doesn\'t return a single movie', () => {
+    cy.intercept({
+      method: 'GET',
+      url: 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/627290'
+    },
+    {
+      status: 500,
+      ok: false,
+      body: { 
+        message: `Our servers are down.` 
+      }
+    })
+
+    cy.visit('http://localhost:3000/movies/627290')
+    cy.url().should('eq', 'http://localhost:3000/not-found')
+  })
+
+  it('should display an error message if the server doesn\'t return all movies', () => {
+    cy.intercept({
+      method: 'GET',
+      url: 'https://rancid-tomatillos.herokuapp.com/api/v2/movies'
+    },
+    {
+      status: 500,
+      ok: false,
+      body: { 
+        message: `Our servers are down.` 
+      }
+    })
+
+    // cy.visit('http://localhost:3000/movies')
+    //   .get('h2').contains('404')
   })
 })
