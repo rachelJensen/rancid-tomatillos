5f7f16bf5b02d77775fd3a90e002bf8ae57ec6ae
Rachel Jensen <rachelangela@gmail.com>
2021-09-06 18:19:12 -0600

    Refactor for 400 and 500 errors

--- src/components/Error404/Error404.js
+++ src/components/Error404/Error404.js
@@ -1,13 +1,16 @@
 import './Error404.css';
 import {Link} from 'react-router-dom';
 
-const Error404 = () => {
+const Error404 = ({ errorType }) => { 
+  let line1 = (errorType === 'error 404') ? '404' : 'Something went wrong';
+  let line2 = (errorType === 'error 404') ? 'Page Not Found' : 'Please try again later'
+  
   return (
       <Link to="*" >
         <section className="error">
           <div className="error-message">
-            <h2 className='neon-error'>404</h2>
-            <h3 className='neon-error'>Page Not Found</h3>
+            <h2 className='neon-error'>{line1}</h2>
+            <h3 className='neon-error'>{line2}</h3>
           </div>
         </section>
       </Link>
