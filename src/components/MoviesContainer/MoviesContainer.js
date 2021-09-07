4242a7138d4198d0d8cb52053ae3631ddbd304d4
Rachel Jensen <rachelangela@gmail.com>
2021-09-06 18:19:32 -0600

    Update Error component props

--- src/components/MoviesContainer/MoviesContainer.js
+++ src/components/MoviesContainer/MoviesContainer.js
@@ -4,6 +4,8 @@ import Error404 from '../Error404/Error404';
 import PropTypes from 'prop-types';
 
 const MoviesContainer = ({ movies, loadingError}) => {
+
+
   const movieCards = movies.map((movie) => {
     return (
       <MoviePoster
@@ -14,7 +16,6 @@ const MoviesContainer = ({ movies, loadingError}) => {
         rating={movie.average_rating}
         release={movie.release_date}
         key={movie.id}
-        // findMovie={findMovie}
       />
     );
   });
@@ -22,14 +23,13 @@ const MoviesContainer = ({ movies, loadingError}) => {
 
 
   return (
-    !loadingError ?
+    (!loadingError) ?
     <div className="background">
       <section className="container">
         {movieCards}
       </section>
     </div>
-    :
-    <Error404 />
+    : <Error404 errorType={loadingError}/>
   )
 };
 
